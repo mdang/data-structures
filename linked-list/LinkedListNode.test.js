5f558cc5460c1c307b17b77b4c31cd22751dbc9c
Mike Dang <mike.dang@generalassemb.ly>
2019-04-23 14:22:11 -0500

    LinkedNode passing tests

--- linked-list/LinkedListNode.test.js
+++ linked-list/LinkedListNode.test.js
@@ -8,15 +8,6 @@ describe('LinkedListNode', () => {
     expect(node.next).toBeNull();
   });
 
-  it('should create list node with object as a value', () => {
-    const nodeValue = { value: 1, key: 'test' };
-    const node = new LinkedListNode(nodeValue);
-
-    expect(node.value.value).toBe(1);
-    expect(node.value.key).toBe('test');
-    expect(node.next).toBeNull();
-  });
-
   it('should link nodes together', () => {
     const node2 = new LinkedListNode(2);
     const node1 = new LinkedListNode(1, node2);
@@ -34,13 +25,5 @@ describe('LinkedListNode', () => {
 
     node.value = 'string value';
     expect(node.toString()).toBe('string value');
-  });
-
-  it('should convert node to string with custom stringifier', () => {
-    const nodeValue = { value: 1, key: 'test' };
-    const node = new LinkedListNode(nodeValue);
-    const toStringCallback = value => `value: ${value.value}, key: ${value.key}`;
-
-    expect(node.toString(toStringCallback)).toBe('value: 1, key: test');
   });
 });
