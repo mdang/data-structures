ff1be5226f618c8adca93dbf5f9fcfe9520b367f
Mike Dang <mike.dang@generalassemb.ly>
2019-04-23 17:07:34 -0500

    Working stack implementation

--- stack/Stack.js
+++ stack/Stack.js
@@ -2,26 +2,33 @@ import LinkedList from '../linked-list/LinkedList'
 
 export default class Stack {
   constructor() {
-
+    this.linkedList = new LinkedList()
   }
 
-  push() {
+  push(value) {
+    this.linkedList.append(value)
 
+    return this
   }
 
   pop() {
-
+    const removedTail = this.linkedList.deleteTail()
+    return removedTail ? removedTail.value : null
   }
 
   peek() {
-
+    if (this.isEmpty()) {
+      return null
+    }
+    
+    return this.linkedList.tail.value
   }
 
   isEmpty() {
-
+    return !this.linkedList.head
   }
 
   toString() {
-
+    return this.linkedList.toString()
   }
 } 
