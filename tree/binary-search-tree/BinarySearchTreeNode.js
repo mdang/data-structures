f34dbc758a62bd184dd2db87a47edfda53b9f902
Mike Dang <mike.dang@generalassemb.ly>
2019-05-02 12:38:12 -0500

    Base of BinarySearchTreeNode

--- tree/binary-search-tree/BinarySearchTreeNode.js
+++ tree/binary-search-tree/BinarySearchTreeNode.js
@@ -1,5 +1,50 @@
 import BinaryTreeNode from '../BinaryTreeNode'
 
 export default class BinarySearchTreeNode extends BinaryTreeNode {
-  
+  constructor(value = null) {
+    super(value)
+  }
+
+  insert(value = null) {
+    if (!this.value) {
+      this.value = value
+
+      return this
+    }
+
+    // left
+    if (value < this.value) {
+      // is there already a left node? 
+      if (this.left) {
+        return this.left.insert(value)
+      }
+      
+      const newNode = new BinarySearchTreeNode(value)
+      this.setLeft(newNode)
+
+      return newNode
+    }
+
+    // right
+    if (value > this.value) {
+      if (this.right) {
+        return this.right.insert(value)
+      }
+
+      const newNode = new BinarySearchTreeNode(value)
+      this.setRight(newNode)
+
+      return newNode
+    }
+
+    return this
+  }
+
+  find(value) {
+
+  }
+
+  contains(value) {
+
+  }
 }
---
+++ tree/binary-search-tree/BinarySearchTreeNode.test.js
@@ -0,0 +1,84 @@
+import BinarySearchTreeNode from './BinarySearchTreeNode';
+
+describe('BinarySearchTreeNode', () => {
+  it('should create binary search tree', () => {
+    const bstNode = new BinarySearchTreeNode(2);
+
+    expect(bstNode.value).toBe(2);
+    expect(bstNode.left).toBeNull();
+    expect(bstNode.right).toBeNull();
+  });
+
+  it('should insert in itself if it is empty', () => {
+    const bstNode = new BinarySearchTreeNode();
+    bstNode.insert(1);
+
+    expect(bstNode.value).toBe(1);
+    expect(bstNode.left).toBeNull();
+    expect(bstNode.right).toBeNull();
+  });
+
+  it('should insert nodes in correct order', () => {
+    const bstNode = new BinarySearchTreeNode(2);
+    const insertedNode1 = bstNode.insert(1);
+
+    expect(insertedNode1.value).toBe(1);
+    expect(bstNode.toString()).toBe('1,2');
+    expect(bstNode.contains(1)).toBe(true);
+    expect(bstNode.contains(3)).toBe(false);
+
+    const insertedNode2 = bstNode.insert(3);
+
+    expect(insertedNode2.value).toBe(3);
+    expect(bstNode.toString()).toBe('1,2,3');
+    expect(bstNode.contains(3)).toBe(true);
+    expect(bstNode.contains(4)).toBe(false);
+
+    bstNode.insert(7);
+
+    expect(bstNode.toString()).toBe('1,2,3,7');
+    expect(bstNode.contains(7)).toBe(true);
+    expect(bstNode.contains(8)).toBe(false);
+
+    bstNode.insert(4);
+
+    expect(bstNode.toString()).toBe('1,2,3,4,7');
+    expect(bstNode.contains(4)).toBe(true);
+    expect(bstNode.contains(8)).toBe(false);
+
+    bstNode.insert(6);
+
+    expect(bstNode.toString()).toBe('1,2,3,4,6,7');
+    expect(bstNode.contains(6)).toBe(true);
+    expect(bstNode.contains(8)).toBe(false);
+  });
+
+  it('should not insert duplicates', () => {
+    const bstNode = new BinarySearchTreeNode(2);
+    bstNode.insert(1);
+
+    expect(bstNode.toString()).toBe('1,2');
+    expect(bstNode.contains(1)).toBe(true);
+    expect(bstNode.contains(3)).toBe(false);
+
+    bstNode.insert(1);
+
+    expect(bstNode.toString()).toBe('1,2');
+    expect(bstNode.contains(1)).toBe(true);
+    expect(bstNode.contains(3)).toBe(false);
+  });
+
+  it('should find node', () => {
+    const node = new BinarySearchTreeNode(10);
+
+    node.insert(20);
+    node.insert(30);
+    node.insert(5);
+    node.insert(40);
+    node.insert(1);
+
+    expect(node.find(6)).toBeNull();
+    expect(node.find(5)).not.toBeNull();
+    expect(node.find(5).value).toBe(5);
+  });
+});
